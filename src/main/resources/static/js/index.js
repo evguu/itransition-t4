13a868e405dcde470beaa63c6820d00151201b66
Litnine <savchenko2002je@gmail.com>
2021-07-24 14:56:47 +0300

    checkboxes fully work

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,17 +1,27 @@
-/*$('.table > tbody > tr').click(function() {
-    console.log(this);
+$(".table > thead :checkbox").click(function() {
+    let input = $(this);
+    let newCondition = input.prop('checked');
+    $(".table > tbody :checkbox").prop('checked', newCondition);
 });
 
-$(".table > tbody > tr > input:checkbox").click(function() {
-    this.prop('checked', !this.prop('checked'));
-});*/
-
-$(":checkbox").click(function() {
-    var $input = $(this);
-    $input.prop('checked', !$input.prop('checked'));
+$(".table > tbody :checkbox").click(function() {
+    let input = $(this);
+    input.prop('checked', !input.prop('checked'));
 });
 
 $('.table > tbody > tr').click(function() {
-    var $input = $("th > input", this);
-    $input.prop('checked', !$input.prop('checked'));
+    let input = $("th > input", this);
+    input.prop('checked', !input.prop('checked'));
+    setHeadCheckboxOnIfAllSet();
 });
+
+function setHeadCheckboxOnIfAllSet(){
+    let headCheckbox = $(".table > thead :checkbox");
+    let areAllSet = true;
+    $(".table > tbody :checkbox").each((k,v) => {
+        if(!$(v).prop('checked')) {
+            areAllSet = false;
+        }
+    });
+    headCheckbox.prop('checked', areAllSet);
+}
