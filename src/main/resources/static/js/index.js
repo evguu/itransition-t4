f37c6b2b01a7851c8b030c6c5ec91bef1ed90184
Litnine <savchenko2002je@gmail.com>
2021-07-24 17:22:00 +0300

    b-table connected

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,31 +1,3 @@
-$(".table > thead :checkbox").click(function() {
-    let input = $(this);
-    let newCondition = input.prop('checked');
-    $(".table > tbody :checkbox").prop('checked', newCondition);
-});
-
-$(".table > tbody :checkbox").click(function() {
-    let input = $(this);
-    input.prop('checked', !input.prop('checked'));
-});
-
-$('.table > tbody > tr').click(function() {
-    let input = $("th > input", this);
-    input.prop('checked', !input.prop('checked'));
-    setHeadCheckboxOnIfAllSet();
-});
-
-function setHeadCheckboxOnIfAllSet(){
-    let headCheckbox = $(".table > thead :checkbox");
-    let areAllSet = true;
-    $(".table > tbody :checkbox").each((k,v) => {
-        if(!$(v).prop('checked')) {
-            areAllSet = false;
-        }
-    });
-    headCheckbox.prop('checked', areAllSet);
-}
-
 function getListOfCheckedRowIds(){
     $(".table > tbody > tr").each((k,v) => {
 
