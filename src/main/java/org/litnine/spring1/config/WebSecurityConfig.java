b61ba1e2d185484f26ce1296116e7bdd6d54ae0e
Litnine <savchenko2002je@gmail.com>
2021-07-25 12:14:04 +0300

    login page redesign

--- src/main/java/org/litnine/spring1/config/WebSecurityConfig.java
+++ src/main/java/org/litnine/spring1/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package org.litnine.spring1.config;
 
-
 import org.litnine.spring1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
